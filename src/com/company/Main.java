ab966ef4745b436879f4f7e1edd45e1c2437d65d
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-03-27 23:27:50 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s= "alok";
+        s=s.concat("singh");
+        System.out.println(s);
+	// write your code here
+    }
+}
